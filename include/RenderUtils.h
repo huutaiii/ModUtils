18537f05b68ed13a4a8e2afb224811ab72c7171b
huutai <truongtai137@gmail.com>
2024-01-26 14:31:53 +0700

    add TAA jitter generator

---
+++ include/RenderUtils.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <vector>
+
+#include "MathUtils.h"
+
+// sub-pixel jitter generator for TAA
+class UJitterGenerator
+{
+protected:
+	std::vector<glm::vec2> Sequence{ 0 };
+	uint32_t Offset; // index of first sample
+	uint32_t Distance; // distance between samples
+	uint32_t NumPhases;
+
+	// https://en.wikipedia.org/wiki/Halton_sequence
+	float HaltonSample(uint32_t index, uint32_t base)
+	{
+		float f = 1.f;
+		float r = 0.f;
+		for (int i = index; i > 0; i = i / base)
+		{
+			f = f / base;
+			r = r + f * (i % base);
+		}
+		return r;
+	}
+
+	void Generate()
+	{
+		Sequence.resize(NumPhases);
+		for (int i = 0; i < NumPhases; ++i)
+		{
+			float x = HaltonSample(i * Distance + Offset, 2);
+			float y = HaltonSample(i * Distance + Offset, 3);
+			Sequence[i] = glm::vec2(x, y) - glm::vec2(0.5f);
+		}
+	}
+
+public:
+
+	UJitterGenerator(uint32_t numPhases, uint32_t offset = 1, uint32_t distance = 1)
+		: NumPhases(numPhases), Offset(offset), Distance(distance)
+	{
+		Generate();
+	}
+
+	glm::vec2 Get(size_t index) const
+	{
+		return Sequence[index % NumPhases];
+	}
+
+	const glm::vec2& operator[](size_t index) const
+	{
+		return Sequence[index % NumPhases];
+	}
+
+	const uint32_t GetNumPhases() const { return NumPhases; }
+
+	// calculates a recommended jitter phase count for use with temporal super sampling
+	// https://github.com/NVIDIA/DLSS/blob/v3.5.10/doc/DLSS_Programming_Guide_Release.pdf
+	inline static uint32_t CalcNumPhasesTSR(uint32_t targetHeight, uint32_t renderHeight, uint32_t baseCount = 8)
+	{
+		return baseCount * (uint32_t)pow2(1.f * targetHeight / renderHeight);
+	}
+};
+
+
